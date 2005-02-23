cf576d323e605122b337c64327ca047c999c7454
Ashley Yakeley <ashley@semantic.org>
2005-02-23 02:48:25 -0800

    get TAI to compile
    darcs-hash:20050223104825-ac6dd-0a3e7a4109ecca08fb312f83deca2257b815310e

--- System/Time/TAI.hs
+++ System/Time/TAI.hs
@@ -13,21 +13,24 @@ module System.Time.TAI
 
 import System.Time.Clock
 
--- | TAI
-type AbsoluteTime = MkAbsoluteTime Integer
+-- | TAI as DiffTime from epoch
+newtype AbsoluteTime = MkAbsoluteTime DiffTime deriving (Eq,Ord)
 
 addAbsoluteTime :: DiffTime -> AbsoluteTime -> AbsoluteTime
+addAbsoluteTime t (MkAbsoluteTime a) = MkAbsoluteTime (t + a)
 
 diffAbsoluteTime :: AbsoluteTime -> AbsoluteTime -> DiffTime
+diffAbsoluteTime (MkAbsoluteTime a) (MkAbsoluteTime b) = a - b
 
 -- | TAI - UTC during this day
-type LeapSecondTable = ModJulianDay -> Int
+type LeapSecondTable = ModJulianDay -> Integer
 
 utcDayLength :: LeapSecondTable -> ModJulianDay -> DiffTime
 utcDayLength table day = siSecondsToTime (86400 + (table (day + 1)) - (table day))
 
-utcToTAITime :: LeapSecondTable -> UTCTime -> TAITime
-utcToTAITime table (UTCTime day dtime) = siSecondsToTime (table day) + 
+utcToTAITime :: LeapSecondTable -> UTCTime -> AbsoluteTime
+utcToTAITime table (UTCTime day dtime) = MkAbsoluteTime
+	((siSecondsToTime (day * 86400 + (table day))) + dtime)
 
-taiToUTCTime :: LeapSecondTable -> TAITime -> UTCTime
-
+taiToUTCTime :: LeapSecondTable -> AbsoluteTime -> UTCTime
+taiToUTCTime table (MkAbsoluteTime t) = undefined
