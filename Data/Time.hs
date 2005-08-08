e3f3a0377025ebf746274e31f0651138350fa6d1
Ashley Yakeley <ashley@semantic.org>
2005-08-08 01:45:48 -0700

    add missing file
    darcs-hash:20050808084548-ac6dd-ed5644c971cd295aeb8602214cacbe198fc0e4e8

---
+++ Data/Time.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -Wall -Werror #-}
+
+module Data.Time
+(
+	module Data.Time.Calendar,
+	module Data.Time.Clock,
+	module Data.Time.LocalTime
+) where
+
+import Data.Time.Calendar
+import Data.Time.Clock
+import Data.Time.LocalTime
