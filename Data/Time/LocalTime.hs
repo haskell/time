70544be1f29f912a032c1ba364b343d621faaa03
Ashley Yakeley <ashley@semantic.org>
2005-08-08 01:44:55 -0700

    add missing file
    darcs-hash:20050808084455-ac6dd-f30c61c5f69f22ce1764c10640dec0e564a68d01

---
+++ Data/Time/LocalTime.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -Wall -Werror #-}
+
+module Data.Time.LocalTime
+(
+	module Data.Time.LocalTime.TimeZone,
+	module Data.Time.LocalTime.TimeOfDay,
+	module Data.Time.LocalTime.LocalTime,
+	module Data.Time.LocalTime.Format
+) where
+
+import Data.Time.LocalTime.TimeZone
+import Data.Time.LocalTime.TimeOfDay
+import Data.Time.LocalTime.LocalTime
+import Data.Time.LocalTime.Format
