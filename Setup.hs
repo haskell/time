843ed89f8f3d46ddabe04f1ea8ccf33426a13c92
Ashley Yakeley <ashley@semantic.org>
2006-12-20 02:12:54 -0800

    attempt to get cabal test working
    darcs-hash:20061220101254-ac6dd-56f6ff37f578c96f352ca61032eaf269e727bdf9

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,21 @@
 module Main (main) where
 
-import Distribution.Simple (defaultMainWithHooks, defaultUserHooks)
+import Distribution.Simple
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+import System.Exit
+import System.Cmd
+import System.Directory
+import Control.Exception
+
+withCurrentDirectory :: FilePath -> IO a -> IO a
+withCurrentDirectory path f = do
+	cur <- getCurrentDirectory
+	setCurrentDirectory path
+	finally f (setCurrentDirectory cur)
+
+runTestScript :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ExitCode
+runTestScript args flag pd lbi = withCurrentDirectory "test" (system "make")
 
 main :: IO ()
-main = defaultMainWithHooks defaultUserHooks
+main = defaultMainWithHooks defaultUserHooks{runTests = runTestScript}
