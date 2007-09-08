635d902b2a9ddcece575ae4b2873f793201e5684
sven.panne <sven.panne@aedion.de>
2007-09-08 09:02:56 -0700

    Fixed Cabal-induced breakage, once again...
    MERGE TO STABLE (if we have a concept of "stable libraries")
    
    darcs-hash:20070908160256-96103-c5c21e89632939ac5896cf44abc9aa381df7ed4e

--- Setup.hs
+++ Setup.hs
@@ -2,13 +2,11 @@ module Main (main) where
 
 import Control.Exception
 import Distribution.PackageDescription
-import Distribution.Setup
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Utils
 import System.Cmd
 import System.Directory
-import System.Info
 
 main :: IO ()
 main = do let hooks = defaultUserHooks { runTests = runTestScript }
@@ -23,6 +21,3 @@ withCurrentDirectory path f = do
 runTestScript :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
 runTestScript _args _flag _pd _lbi
  = maybeExit $ withCurrentDirectory "test" $ system "make"
-
-type ConfHook = PackageDescription -> ConfigFlags -> IO LocalBuildInfo
-
