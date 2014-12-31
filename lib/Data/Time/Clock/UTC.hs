5bcf96f4c0f5755a110e7ef6ae3ab54b42a1e96e
Tom Ellis <tom-2013@purelyagile.com>
2014-12-31 13:05:27 +0000

    Document that the show instance for UTCTime is elsewhere.

--- lib/Data/Time/Clock/UTC.hs
+++ lib/Data/Time/Clock/UTC.hs
@@ -28,6 +28,9 @@ import Data.Data
 -- | This is the simplest representation of UTC.
 -- It consists of the day number, and a time offset from midnight.
 -- Note that if a day has a leap second added to it, it will have 86401 seconds.
+--
+-- For the 'Show' instance of 'UTCTime' import @Data.Time@ or
+-- @Data.Time.LocalTime@.
 data UTCTime = UTCTime {
     -- | the day
     utctDay :: Day,
