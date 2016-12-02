ff9488f7dd6d5a6bf26582fd02b73dd5bae08646
winterland1989 <winterland1989@gmail.com>
2016-12-02 10:16:22 +0800

    seperate bench project

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,22 +1,47 @@
+{-# LANGUAGE PackageImports #-}
 module Main where
 
+import  Criterion.Main
+import  Data.Time.Clock
+import  Data.Time.Calendar
+import  Data.Time.Clock.POSIX
+import  Data.Time.LocalTime
+import  Data.Time.Format
+import  Data.Time.Clock
 
-import     Criterion.Main
-import     Data.Time.Clock
-import     Data.Time.Clock.POSIX
-import     Data.Time.LocalTime
+import qualified "time" Data.Time.Clock       as O
+import qualified "time" Data.Time.Clock.POSIX as O
+import qualified "time" Data.Time.LocalTime   as O
+import qualified "time" Data.Time.Format      as O
+import qualified "time" Data.Time.Clock       as O
 
 main :: IO ()
 main = do
     getCurrentTime >>= print
-    getPOSIXTime >>= print . posixSecondsToUTCTime
+    O.getCurrentTime >>= print
+    getPOSIXTime >>= print . posixToUTCTime
+    O.getPOSIXTime >>= print . O.posixSecondsToUTCTime
     getZonedTime >>= print
+    O.getZonedTime >>= print
+
     tz <- getCurrentTimeZone
+    ct <- getCurrentTime
+    otz <- O.getCurrentTimeZone
+    oct <- O.getCurrentTime
+
     defaultMain
-        [ bgroup "time"
+        [ bgroup "new"
             [ bench "getCurrentTime" $ nfIO getCurrentTime
             , bench "getPOSIXTime" $ nfIO getPOSIXTime
             , bench "getZonedTime" $ nfIO getZonedTime
+            , bench "formatTime" $ nf (formatTime defaultTimeLocale "%a, %_d %b %Y %H:%M:%S %Z") ct
+            ]
+        ,
+          bgroup "old"
+            [ bench "getCurrentTime" $ nfIO O.getCurrentTime
+            , bench "getPOSIXTime" $ nfIO O.getPOSIXTime
+            , bench "getZonedTime" $ nfIO O.getZonedTime
+            , bench "formatTime" $ nf (O.formatTime O.defaultTimeLocale "%a, %_d %b %Y %H:%M:%S %Z") oct
             ]
         ]
 
