e71ecc300deee5782773a434769d3dc04e2e3831
Ashley Yakeley <ashley@yakeley.org>
2017-02-03 14:02:22 -0800

    test: minor clean-up

--- test/main/Main.hs
+++ test/main/Main.hs
@@ -1,7 +1,50 @@
 module Main where
-import Test.Tasty
-import Test.Tests
+
 import Foreign.C.Types
+import Test.Tasty
+import Test.Calendar.AddDays
+import Test.Calendar.Calendars
+import Test.Calendar.ClipDates
+import Test.Calendar.ConvertBack
+import Test.Calendar.Easter
+import Test.Calendar.LongWeekYears
+import Test.Calendar.MonthDay
+import Test.Calendar.Valid
+import Test.Clock.Conversion
+import Test.Clock.Resolution
+import Test.Clock.TAI
+import Test.Format.Format
+import Test.Format.ParseTime
+import Test.LocalTime.Time
+import Test.LocalTime.TimeZone
+
+
+tests :: TestTree
+tests = testGroup "Time" [
+    testGroup "Calendar" [
+        addDaysTest,
+        testCalendars,
+        clipDates,
+        convertBack,
+        longWeekYears,
+        testMonthDay,
+        testEaster,
+        testValid
+        ],
+    testGroup "Clock" [
+        testClockConversion,
+        testResolution,
+        testTAI
+        ],
+    testGroup "Format" [
+        testFormat,
+        testParseTime
+        ],
+    testGroup "LocalTime" [
+        testTime,
+        testTimeZone
+        ]
+    ]
 
 main :: IO ()
 main = do
