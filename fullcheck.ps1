1fa600afa19381aa17ca219055e6247708305a90
Ashley Yakeley <ashley@semantic.org>
2021-01-08 17:44:19 -0800

    fullcheck: call autoreconf (#148)

--- fullcheck.ps1
+++ fullcheck.ps1
@@ -1,4 +1,6 @@
 $ErrorActionPreference = "Stop"
+& "autoreconf" "-i"
+if (!$?) {Exit 1}
 ForEach ($r in "lts-9","lts-11","lts-12","lts-14","lts-16","nightly")
 {
 	& "stack" "--resolver" "$r" "build"
