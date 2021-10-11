911a0fa9ad380159ecf815414e50730161c99f44
Ashley Yakeley <ashley@semantic.org>
2021-10-10 18:15:35 -0700

    Prepare for 1.13 release

--- fullcheck.ps1
+++ fullcheck.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 & "git" "pull"
 & "stack" "exec" "--" "env" "autoreconf" "-i"
 if (!$?) {Exit 1}
-ForEach ($c in "ghc-8.8.4","ghc-8.10.5","ghc-9.0.1")
+ForEach ($c in "ghc-8.8.4","ghc-8.10.7","ghc-9.0.1")
 {
 	& "stack" "--compiler" "$c" "build"
 	if (!$?) {Exit 1}
