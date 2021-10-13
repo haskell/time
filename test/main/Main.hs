3c46c8ea93159d971cb0514b6a38e1ef0ef2cdb9
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2021-10-13 15:42:13 -0500

    [#179] Add property and unit tests for DayPeriod functions
    Co-authored-by: Felix Mino <fmino@stackbuilders.com>

--- test/main/Main.hs
+++ test/main/Main.hs
@@ -39,7 +39,7 @@ tests =
             , clipDates
             , convertBack
             , longWeekYears
-            , testHasDays
+            , testDayPeriod
             , testMonthDay
             , testMonthOfYear
             , testEaster
