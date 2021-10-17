96c1bc12cf9775103d62c59e8ad6dba04d5581cc
Ashley Yakeley <ashley@semantic.org>
2021-10-16 20:59:59 -0700

    Build: use cabal instead of stack

--- fullcheck.ps1
+++ fullcheck.ps1
@@ -1,11 +1,25 @@
 $ErrorActionPreference = "Stop"
 & "git" "clean" "-dXf"
+if (!$?) {Exit 1}
 & "git" "pull"
+if (!$?) {Exit 1}
 & "stack" "exec" "--" "env" "autoreconf" "-i"
+if (!$?) {Exit 1}
+& "ghcup" "upgrade"
+if (!$?) {Exit 1}
+& "ghcup" "install" "cabal" "--set" "latest"
 if (!$?) {Exit 1}
 ForEach ($c in "ghc-8.8.4","ghc-8.10.7","ghc-9.0.1")
 {
-	& "stack" "--compiler" "$c" "build"
-	if (!$?) {Exit 1}
+    & "ghcup" "install" "ghc" "--set" "$c"
+    if (!$?) {Exit 1}
+    & "cabal" "update"
+    if (!$?) {Exit 1}
+    & "cabal" "v1-install" "--only-dependencies" "--enable-tests"
+    if (!$?) {Exit 1}
+    & "cabal" "v1-configure" "--enable-tests"
+    if (!$?) {Exit 1}
+    & "cabal" "v1-test"
+    if (!$?) {Exit 1}
 }
 Echo "OK"
