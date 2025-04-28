5ebf69877fae94fe6b9db08f0b3015bbe59ed277
Ashley Yakeley <ashley@semantic.org>
2025-04-27 19:01:05 -0700

    test for clock resolution

--- test/ForeignCalls.hs
+++ test/ForeignCalls.hs
@@ -1,24 +1,30 @@
 module Main (main) where
 
 import Control.Exception
+import Control.Monad
 import Data.Monoid
+import Data.Foldable
 import Data.Traversable
 import System.Exit
 import System.IO
 import Data.Time
 import Data.Time.Clock.POSIX
 import Data.Time.Clock.System
+import Data.Time.Clock.TAI
 
 data Test = MkTest String (IO ())
 
 tests :: [Test]
 tests =
-    [ MkTest "getCurrentTime" $ getCurrentTime >> return ()
-    , MkTest "getZonedTime" $ getZonedTime >> return ()
-    , MkTest "getCurrentTimeZone" $ getCurrentTimeZone >> return ()
-    , MkTest "getTimeZone" $ getCurrentTime >>= getTimeZone >> return ()
-    , MkTest "getPOSIXTime" $ getPOSIXTime >> return ()
-    , MkTest "getSystemTime" $ getSystemTime >> return ()
+    [ MkTest "getCurrentTime" $ void $ getCurrentTime
+    , MkTest "getZonedTime" $ void $ getZonedTime
+    , MkTest "getCurrentTimeZone" $ void $ getCurrentTimeZone
+    , MkTest "getTimeZone" $ void $ getCurrentTime >>= getTimeZone
+    , MkTest "getPOSIXTime" $ void $ getPOSIXTime
+    , MkTest "getSystemTime" $ void $ getSystemTime
+    , MkTest "getTime_resolution" $ void $ evaluate getTime_resolution
+    , MkTest "taiClock time" $ for_ taiClock $ \(_, getTime) -> void $ getTime
+    , MkTest "taiClock resolution" $ for_ taiClock $ \(res, _) -> void $ evaluate res
     ]
 
 runTest :: Test -> IO Bool
