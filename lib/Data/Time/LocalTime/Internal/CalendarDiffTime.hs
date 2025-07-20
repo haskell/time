0cb9c55a10cab764ba045020cf6cd6c02311bb8b
Ashley Yakeley <ashley@semantic.org>
2025-07-19 19:59:20 -0700

    [add/diff]UTCDuration[Clip/Rollover] (#259)

--- lib/Data/Time/LocalTime/Internal/CalendarDiffTime.hs
+++ lib/Data/Time/LocalTime/Internal/CalendarDiffTime.hs
@@ -7,8 +7,11 @@ module Data.Time.LocalTime.Internal.CalendarDiffTime (
 
 import Control.DeepSeq
 import Data.Data
+import Data.Time.Calendar.Gregorian
 import Data.Time.Calendar.CalendarDiffDays
 import Data.Time.Clock.Internal.NominalDiffTime
+import Data.Time.Clock.Internal.UTCTime
+import Data.Time.Clock.Internal.UTCDiff
 import GHC.Generics
 import Language.Haskell.TH.Syntax qualified as TH
 
@@ -39,3 +42,23 @@ calendarTimeTime dt = CalendarDiffTime 0 dt
 -- | Scale by a factor. Note that @scaleCalendarDiffTime (-1)@ will not perfectly invert a duration, due to variable month lengths.
 scaleCalendarDiffTime :: Integer -> CalendarDiffTime -> CalendarDiffTime
 scaleCalendarDiffTime k (CalendarDiffTime m d) = CalendarDiffTime (k * m) (fromInteger k * d)
+
+addUTCDurationClip :: CalendarDiffTime -> UTCTime -> UTCTime
+addUTCDurationClip (CalendarDiffTime m d) (UTCTime day t) =
+    addUTCTime d $ UTCTime (addGregorianMonthsClip m day) t
+
+addUTCDurationRollOver :: CalendarDiffTime -> UTCTime -> UTCTime
+addUTCDurationRollOver (CalendarDiffTime m d) (UTCTime day t) =
+    addUTCTime d $ UTCTime (addGregorianMonthsRollOver m day) t
+
+diffUTCDurationClip :: UTCTime -> UTCTime -> CalendarDiffTime
+diffUTCDurationClip (UTCTime day1 t1) (UTCTime day2 t2) =
+    let
+    CalendarDiffTime m t = calendarTimeDays $ diffGregorianDurationClip day1 day2
+    in CalendarDiffTime m $ t + realToFrac (t1 - t2)
+
+diffUTCDurationRollOver :: UTCTime -> UTCTime -> CalendarDiffTime
+diffUTCDurationRollOver (UTCTime day1 t1) (UTCTime day2 t2) =
+    let
+    CalendarDiffTime m t = calendarTimeDays $ diffGregorianDurationRollOver day1 day2
+    in CalendarDiffTime m $ t + realToFrac (t1 - t2)
