5fd5ee745d53d68baa5dd3ad8ff45fa955f56347
Ashley Yakeley <ashley@semantic.org>
2025-07-20 22:23:45 -0700

    Patterns for DiffTime & NominalDiffTime

--- test/main/Test/Clock/Pattern.hs
+++ test/main/Test/Clock/Pattern.hs
@@ -19,11 +19,13 @@ testClockPatterns =
                 , testSame "Seconds-Picoseconds" (Picoseconds 7_276_000_000_000) (Seconds 7.276)
                 , testSame "Minutes-Seconds" (Seconds 210) (Minutes 3.5)
                 , testSame "Hours-Minutes" (Minutes 120) (Hours 2)
+                , testSame "Nominal" 37.4 (Nominal 37.4)
                 ],
             testGroup "deconstruct"
                 [ testSame "Seconds" 52.4 ((\(Seconds x) -> x) 52.4)
                 , testSame "Seconds-Picoseconds" 7.276 ((\(Seconds x) -> x) $ Picoseconds 7_276_000_000_000)
                 , testSame "Minutes-Seconds" 3.5 ((\(Minutes x) -> x) $ Seconds 210)
                 , testSame "Hours-Minutes" 2 ((\(Hours x) -> x) $ Minutes 120)
+                , testSame "Nominal" 37.4 ((\(Nominal x) -> x) 37.4)
                 ]
             ]
