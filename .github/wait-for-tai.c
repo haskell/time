341c71c6ead283138860f74c5c1ee2fb8d30731c
Ashley Yakeley <ashley@semantic.org>
2026-04-04 17:44:15 -0700

    wait-for-tai: show elapsed time on success

--- .github/wait-for-tai.c
+++ .github/wait-for-tai.c
@@ -21,8 +21,10 @@ int main(void)
     start = time(NULL);
     for (;;) {
         struct timespec ts;
+        long elapsed = difftime(time(NULL), start);
+
         if (clock_gettime(CLOCK_TAI, &ts) == 0) {
-            printf("CLOCK_TAI is usable: %lld.%09ld\n", (long long) ts.tv_sec, ts.tv_nsec);
+            printf("CLOCK_TAI is usable after %ld seconds: %lld.%09ld\n", elapsed, (long long) ts.tv_sec, ts.tv_nsec);
             return 0;
         }
 
@@ -31,7 +33,7 @@ int main(void)
             return 1;
         }
 
-        if (difftime(time(NULL), start) >= timeout_seconds) {
+        if (elapsed >= timeout_seconds) {
             fprintf(stderr, "CLOCK_TAI did not become usable within %ld seconds\n", timeout_seconds);
             return 1;
         }
