3097308079ddf82a2f4bf0f25cdf71e823b7baf7
Ashley Yakeley <ashley@semantic.org>
2026-07-18 21:19:37 -0700

    Test for Easter defect (#298)

--- test/main/Test/Calendar/Easter.hs
+++ test/main/Test/Calendar/Easter.hs
@@ -2,38 +2,47 @@ module Test.Calendar.Easter (
     testEaster,
 ) where
 
+import Data.Foldable
 import Data.Time.Calendar
 import Data.Time.Calendar.Easter
-import Data.Time.Format
-import Test.Calendar.EasterRef
 import Test.Tasty
 import Test.Tasty.HUnit
+import Text.Read
 
---
-days :: [Day]
-days = [ModifiedJulianDay 53000 .. ModifiedJulianDay 53014]
+easterDatesFile :: FilePath
+easterDatesFile = "test/main/Test/Calendar/EasterData/easter500.txt"
 
-showWithWDay :: Day -> String
-showWithWDay = formatTime defaultTimeLocale "%F %A"
+parseField :: Read a => String -> IO a
+parseField field =
+    case readMaybe field of
+        Just value -> return value
+        Nothing -> fail $ "invalid integer field: " ++ show field
+
+parseEasterDates :: [String] -> IO [Day]
+parseEasterDates [] = return []
+parseEasterDates (monthText : dayText : yearText : rest) = do
+    month <- parseField monthText
+    day <- parseField dayText
+    year <- parseField yearText
+    easterDay <-
+        maybe (fail $ "invalid date: " ++ unwords [yearText, monthText, dayText]) return $
+            fromGregorianValid year month day
+    (easterDay :) <$> parseEasterDates rest
+parseEasterDates fields = fail $ "easter500.txt: expected month/day/year triples, leftover fields: " ++ show fields
+
+getEasterDates :: IO [Day]
+getEasterDates = do
+    contents <- readFile easterDatesFile
+    parseEasterDates $ words contents
+
+testGregorianEaster :: TestTree
+testGregorianEaster =
+    testCase "Gregorian Easter" $ do
+        easterDates :: [Day] <- getEasterDates
+        for_ easterDates $ \expectedDate@(YearMonthDay y _ _) -> do
+            let
+                foundDate = gregorianEaster y
+            assertEqual "Gregorian Easter" expectedDate foundDate
 
 testEaster :: TestTree
-testEaster =
-    testCase "testEaster" $
-        let
-            ds = unlines $ map (\day -> unwords [showWithWDay day, "->", showWithWDay (sundayAfter day)]) days
-            f y =
-                unwords
-                    [ show y ++ ", Gregorian: moon,"
-                    , show (gregorianPaschalMoon y) ++ ": Easter,"
-                    , showWithWDay (gregorianEaster y)
-                    ]
-                    ++ "\n"
-            g y =
-                unwords
-                    [ show y ++ ", Orthodox : moon,"
-                    , show (orthodoxPaschalMoon y) ++ ": Easter,"
-                    , showWithWDay (orthodoxEaster y)
-                    ]
-                    ++ "\n"
-        in
-            assertEqual "" testEasterRef $ ds ++ concatMap (\y -> f y ++ g y) [2000 .. 2020]
+testEaster = testGroup "testEaster" [testGregorianEaster]
